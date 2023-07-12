8be6c2bffaa08dab148f6883fb12b3db9027fb40
unknown <carlos.costa.pimentel@gmail.com>
2023-07-12 15:28:45 +0100

    changed sqlite to postgres

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'foodonline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodOnline_db',
+        'USER': 'admin_foodOnline',
+        'PASSWORD': 'Ajax2023#',
+        'HOST': 'localhost',
     }
 }
 
