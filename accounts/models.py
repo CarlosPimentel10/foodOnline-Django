074be8aef3613268ef4ec1928cc7431b7ac54734
unknown <carlos.costa.pimentel@gmail.com>
2023-07-14 19:00:15 +0100

    created signals for userprofile creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -95,3 +97,18 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.email
+
+# django signals
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            UserProfile.objects.create(user=instance)
+            print('Profile did not exist but is now created')
+
+
