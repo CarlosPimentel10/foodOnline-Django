9de899fd38ebd82e75ad27c16f449ec14f9b0566
unknown <carlos.costa.pimentel@gmail.com>
2023-07-25 12:34:15 +0100

    migrated the vendor model

--- foodonline_main/urls.py
+++ foodonline_main/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-    path()
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
