112b0c8f73c2466813af6b6059d8207f43641595
unknown <carlos.costa.pimentel@gmail.com>
2023-08-09 17:15:32 +0100

    configure login, logout, dashboard views and html template

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from .forms import UserForm
 from .models import User, UserProfile
 from vendor.forms import VendorForm
@@ -72,11 +72,26 @@ def registerVendor(request):
     return render(request, 'accounts/registerVendor.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return
+    auth.logout(request)
+    messages.info(request, 'You are logged out')
+    return redirect('login')
 
 def dashboard(request):
-    return
+    return render(request, 'accounts/dashboard.html')
