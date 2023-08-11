6c933300b1fa8ef870bb4a0244ced37e9ef9d53c
unknown <carlos.costa.pimentel@gmail.com>
2023-08-11 11:51:54 +0100

    restricted vendor, customer to their profile and custom 403 message to user

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .forms import UserForm
 from .models import User, UserProfile
 from vendor.forms import VendorForm
 from .utils import detectUser
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+# Restrict vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
 
+# Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+    
 def registerUser(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in.')
@@ -110,9 +123,13 @@ def myAccount(request):
     redirectUrl = detectUser(user)
     return redirect(redirectUrl)
 
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
 def custDashboard(request):
     return render(request, 'accounts/custDashboard.html')
 
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     return render(request, 'accounts/vendorDashboard.html')
 
